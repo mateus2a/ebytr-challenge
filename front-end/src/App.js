0ea6e0f4a3e7a3945dc14d32591408316f547a3f
Mateus <mtauws@gmail.com>
2021-11-02 14:02:07 -0300

    chore: add task

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import TaskList from './pages/TaskList';
+
+import AddTask from './pages/AddTask';
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={TaskList} />
+        <Route exact path="/all" component={TaskList} />
+        <Route exact path="/add" component={AddTask} />
       </Switch>
     </BrowserRouter>
   );
---
+++ front-end/src/pages/AddTask/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { store } from '../../services/api';
+import { useHistory } from 'react-router-dom';
+
+const initialValue = {
+  name: '',
+  status: '',
+}
+
+const AddTask = () => {
+  const [task, setTask] = useState(initialValue);
+  const { name, status } = task;
+  let history = useHistory();
+
+  const onValueChange = (e) => {
+      console.log(e.target.value);
+      setTask({...task, [e.target.name]: e.target.value})
+  }
+
+  const addTaskDetails = async() => {
+      await store(task);
+      history.push('./all');
+  }
+
+  return (
+      <div>
+        <input onChange={(e) => onValueChange(e)} name='name' value={name} />
+
+        <input onChange={(e) => onValueChange(e)} name='status' value={status}  />
+
+        <button variant="contained" color="primary" onClick={() => addTaskDetails()}>Add Task</button>
+      </div>
+  )
+}
+
+export default AddTask;
